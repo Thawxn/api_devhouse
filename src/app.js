7a0f7c2bc8b655c4a522d8b7695499a881dc5611
Thawan Marques <thawan28032001@gmail.com>
2022-12-23 23:26:29 -0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
 import routes from "./routes";
 
 class App{
 
     constructor(){
         this.server = express();
+
+        mongoose.set("strictQuery", true);
+        mongoose.connect("mongodb+srv://thawxn:40028922Th.@devhouse.mzp8dqb.mongodb.net/devhouse?retryWrites=true&w=majority", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
 
         this.middleware();
         this.routes();
