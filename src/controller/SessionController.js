10cbb5aa899ebc1b402c6e5303a3d4037a009e93
Thawan Marques <thawan28032001@gmail.com>
2022-12-27 15:53:17 -0300

    Validação de email e formulario

--- src/controller/SessionController.js
+++ src/controller/SessionController.js
@@ -7,11 +7,20 @@ update: quando queremos listar uma unica sessão
 destroy: quando queremos listr uma unica sessão
 */
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class SessionController{
 
   async store(req, res){
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+    });
+
     const { email } = req.body;
+
+    if(!(await schema.isValid(req.body))){
+      return res.status(400).json({ error: 'Falha na validação'})
+    }
 
     //Verificando se esse usuario já existe
     let user = await User.findOne({ email });
